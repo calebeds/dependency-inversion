2238ae58e7a5f69adf0a45a69ada49501dc20896
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-04-25 11:42:28 -0300

    add dependency inversion example

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,22 @@
+interface DBConnectionInterface {
+    public int connect();
+}
 
+class MySqlConnection implements DBConnectionInterface {
+
+    @Override
+    public int connect() {
+        return 0;
+    }
+}
+
+class PasswordReminder {
+    private int dbConnection;
+
+    public PasswordReminder(DBConnectionInterface dbConnection) {
+        this.dbConnection = dbConnection.connect();
+    }
+}
 
 
 
